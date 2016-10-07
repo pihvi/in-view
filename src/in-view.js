b9fdf826c44a75f20c5c01244f06b0ff7db75117
Cam Wiegert <cam@camwiegert.com>
2016-10-07 16:47:42 -0500

    Remove option methods from singleton

--- src/in-view.js
+++ src/in-view.js
@@ -23,7 +23,7 @@ const inView = () => {
     * Maintain a list of all registries and a default options object.
     */
     const register = [];
-    const options  = { offset: {}, threshold: 0, test: inViewport };
+    const options  = { offset: { top: 0, right: 0, bottom: 0, left: 0 }, threshold: 0, test: inViewport };
 
     /**
     * Check each registry from selector history,
@@ -62,45 +62,10 @@ const inView = () => {
     };
 
     /**
-    * Mutate the offset object with either an object
-    * or a number.
-    */
-    control.offset = o => {
-        if (o === undefined) return options.offset;
-        const isNum = n => typeof n === 'number';
-        ['top', 'right', 'bottom', 'left']
-            .forEach(isNum(o) ?
-                dim => options.offset[dim] = o :
-                dim => isNum(o[dim]) ? options.offset[dim] = o[dim] : null
-            );
-        return options.offset;
-    };
-
-    /**
-    * Set the threshold with a number.
-    */
-    control.threshold = n => {
-        return typeof n === 'number' && n >= 0 && n <= 1
-            ? options.threshold = n
-            : options.threshold;
-    };
-
-    /**
-    * Use a custom test, overriding inViewport, to
-    * determine element visibility.
-    */
-    control.test = fn => {
-        return typeof fn === 'function'
-            ? options.test = fn
-            : options.test;
-    };
-
-    /**
     * Add proxy for test function, set defaults,
     * and return the interface.
     */
     control.is = el => options.test(el, options);
-    control.offset(0);
     return control;
 
 };
